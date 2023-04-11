feccdf3e26993ab8a645d80676e0ba97edc799e0
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-04-11 19:27:49 +0900

    .

--- how-about-idea/src/components/BrainStorming.js
+++ how-about-idea/src/components/BrainStorming.js
@@ -6,445 +6,389 @@ import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-
 const PrintedWordCss = styled.div`
+  width: 25%;
+  height: 25%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
+function PrintedWord(props) {
+  const renew_word = () => {
+    let word_buf = [...props.wordList];
+    let print_buf = [...props.print];
 
-width:25%;
-height:25%;
-display:flex;
-align-items:center;
-justify-content:center;
+    for (let i = 0; i < print_buf.length; i++) {
+      if (props.word === print_buf[i]) print_buf.splice(i, 1);
 
-`
-
-function PrintedWord(props){
-
-    const renew_word=()=>{
-
-        let word_buf=[...props.wordList]
-        let print_buf = [...props.print]
-        
-        for ( let i=0 ; i<print_buf.length; i++){
-
-            if( props.word === print_buf[i])
-                print_buf.splice(i,1)
-
-            if(print_buf[i]===-1){
-                print_buf = print_buf.splice(0,i)
-                break
-            }
-        }
-
-
-        for ( let i=0 ; i<print_buf.length; i++){
-
-            if(props.word === word_buf[i]){
-
-                word_buf.splice(i,1)
-                if(props.json_data[props.word]!==undefined)
-                    word_buf = [...word_buf,...props.json_data[props.word]]
-                break;
-            }
-
-        }
-
-        const len = 16-print_buf.length
-
-        for(let i =0; i<len ;){
-            if(print_buf.length<word_buf.length){
-                while (true){
-
-                    const rand= Math.floor(Math.random() *word_buf.length);
-                    let flag = false;
-                    for(let j=0;j<print_buf.length; j++ ){
-                        if(print_buf[j]===word_buf[rand]){
-                            flag=true
-                            break;
-                        }
-                    }
-        
-                    if(!flag){
-                        print_buf.push(word_buf[rand])
-                        i++
-                        break
-                    }
-        
-                }
-            }
-
-            else{
-
-                print_buf.push(-1)
-                i++
-
-            }
-             
-        }
-
-        props.setPrint([...print_buf])
-        props.setWord([...word_buf])
+      if (print_buf[i] === -1) {
+        print_buf = print_buf.splice(0, i);
+        break;
+      }
     }
 
-    return(
+    for (let i = 0; i < print_buf.length; i++) {
+      if (props.word === word_buf[i]) {
+        word_buf.splice(i, 1);
+        if (props.json_data[props.word] !== undefined)
+          word_buf = [...word_buf, ...props.json_data[props.word]];
+        break;
+      }
+    }
 
-        <PrintedWordCss onClick={()=>{
-            if(props.word!==""){
+    const len = 16 - print_buf.length;
 
-                props.setPrev(props.word)
-                renew_word()
-                
+    for (let i = 0; i < len; ) {
+      if (print_buf.length < word_buf.length) {
+        while (true) {
+          const rand = Math.floor(Math.random() * word_buf.length);
+          let flag = false;
+          console.log(print_buf, word_buf[rand]);
+          for (let j = 0; j < print_buf.length; j++) {
+            if (print_buf[j] === word_buf[rand]) {
+              flag = true;
+              break;
             }
-        
-        }}>
+          }
+          console.log("test 2");
+          if (!flag) {
+            print_buf.push(word_buf[rand]);
+            i++;
+            break;
+          }
+          console.log("test3");
+          debugger;
+        }
+      } else {
+        print_buf.push(-1);
+        i++;
+      }
+    }
 
-            {props.word}
+    props.setPrint([...print_buf]);
+    props.setWord([...word_buf]);
+  };
 
-        </PrintedWordCss>
-
-    )
+  return (
+    <PrintedWordCss
+      onClick={() => {
+        if (props.word !== "") {
+          props.setPrev(props.word);
+          renew_word();
+        }
+      }}
+    >
+      {props.word}
+    </PrintedWordCss>
+  );
 }
 
-
 const NodeCss = styled.div`
-  overflow:hidden;
-  width:28%;
+  overflow: hidden;
+  width: 28%;
   border: 1px solid skyblue;
   border-radius: 12px;
-  margin:1% 1.5%;
-  
-  .wrap{
-    width:200%;
+  margin: 1% 1.5%;
+
+  .wrap {
+    width: 200%;
     display: flex;
-    transform:translate(0%);
-    transition:0.5s;
+    transform: translate(0%);
+    transition: 0.5s;
 
-    .container1,.container2{
+    .container1,
+    .container2 {
       display: flex;
-      width:50%;
+      width: 50%;
       align-items: center;
       justify-content: center;
     }
-    
-    .container1{
 
-      text-align:center;
-
+    .container1 {
+      text-align: center;
     }
 
-    .add,.cancle{
-      width:50%;
+    .add,
+    .cancle {
+      width: 50%;
       display: flex;
       align-items: center;
       justify-content: center;
     }
-    
-    .add{
-      padding:7% 1%;
-      color:#ffffff;
-      background:skyblue;
+
+    .add {
+      padding: 7% 1%;
+      color: #ffffff;
+      background: skyblue;
     }
 
-    .cancle{
-      color:red;
-      padding:5% 1%;
-
+    .cancle {
+      color: red;
+      padding: 5% 1%;
     }
-
   }
+`;
 
+function Node(props) {
+  const menu = useRef();
 
-
-
-`
-
-function Node(props){
-
-  const menu = useRef()
-
-  return(
-
-    <NodeCss >
+  return (
+    <NodeCss>
       <div className="wrap" ref={menu}>
-        <div className="container1" onClick={()=>{menu.current.style.transform="translate(-50%)"}}>
+        <div
+          className="container1"
+          onClick={() => {
+            menu.current.style.transform = "translate(-50%)";
+          }}
+        >
           {props.word}
         </div>
         <div className="container2">
-        
-          <p className="cancle" 
-          onClick={()=>{
-            menu.current.style.transform="translate(0%)"
-            props.setClick("단어를 선택해주세요")
-          }}>취소</p>
+          <p
+            className="cancle"
+            onClick={() => {
+              menu.current.style.transform = "translate(0%)";
+              props.setClick("단어를 선택해주세요");
+            }}
+          >
+            취소
+          </p>
 
-          <p className="add" 
-          onClick={()=>{
-            props.setClick(props.word)
-            menu.current.style.transform="translate(0%)"
-            props.setMenu(true)
-          }}>선택</p>
-        
+          <p
+            className="add"
+            onClick={() => {
+              props.setClick(props.word);
+              menu.current.style.transform = "translate(0%)";
+              props.setMenu(true);
+            }}
+          >
+            선택
+          </p>
         </div>
       </div>
     </NodeCss>
-  )
+  );
 }
 
+const BrainStormingCss = styled.div`
+  width: 100vw;
+  height: 92vh;
+  background: rgba(0, 0, 0, 0.1);
+  display: flex;
 
-const BrainStormingCss = styled.div `
-
-width:100vw;
-height:92vh;
-background:rgba(0,0,0,0.1);
-display:flex;
-
-.word_select{
-    transition:0.3s;
-    width:46vw;
-    height:80vh;
-    background:#ffffff;
+  .word_select {
+    transition: 0.3s;
+    width: 46vw;
+    height: 80vh;
+    background: #ffffff;
     margin: 6vh 2vw;
-    border-radius:12px;
-    display:flex;
-    align-items:center;
-    flex-wrap:wrap;
-}
+    border-radius: 12px;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+  }
 
-.menu{
-
-    width:46vw;
-    height:80vh;
-    background:#ffffff;
+  .menu {
+    width: 46vw;
+    height: 80vh;
+    background: #ffffff;
     margin: 6vh 2vw;
-    border-radius:12px;
+    border-radius: 12px;
 
-    .title{
+    .title {
+      width: 100%;
+      height: 10%;
+      display: flex;
 
-        width:100%;
-        height:10%;
-        display:flex;
+      p {
+        width: 50%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: 0.5s;
+      }
 
-        p{
-            width:50%;
-            height:100%;
-            display:flex;
-            align-items:center;
-            justify-content:center;
-            transition:.5s;
-        }
+      .word {
+        color: ${(props) => (props.menu ? "#00000077" : "#000000")};
+        border-bottom: 1.5px solid
+          ${(props) => (props.menu ? "#00000077" : "#000000")};
+      }
 
-        .word{
-            color:${props=>props.menu?"#00000077":"#000000"};
-            border-bottom:1.5px solid ${props=>props.menu?"#00000077":"#000000"};
-        }
-
-        .tool{
-            color: ${props=>props.menu?"#000000":"#00000077"};
-            border-bottom:1.5px solid ${props=>props.menu?"#000000":"#00000077"};;
-        }
-
+      .tool {
+        color: ${(props) => (props.menu ? "#000000" : "#00000077")};
+        border-bottom: 1.5px solid
+          ${(props) => (props.menu ? "#000000" : "#00000077")};
+      }
     }
 
-    .slider{
-
-        overflow:hidden;
+    .slider {
+      overflow: hidden;
     }
 
+    .container {
+      width: 200%;
+      height: 90%;
+      display: flex;
+      overflow: hidden;
+      transform: translate(${(props) => (props.menu ? -50 : 0)}%);
+      transition: 0.5s;
 
-    .container{
+      .word_container {
+        width: 100%;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        height: 70vh;
+      }
 
-        width:200%;
-        height:90%;
-        display:flex;
-        overflow:hidden;
-        transform: translate(${props=>props.menu?-50:0}%);
-        transition:0.5s;
-        
-        .word_container{
+      .words {
+        margin: 2% 2%;
+        width: 96%;
+        display: flex;
+        flex-wrap: wrap;
+      }
 
-            width:100%;
-            overflow-x:hidden;
-            overflow-y:scroll;
-            height:70vh;
+      .tools {
+        margin: 2% 2%;
+        width: 96%;
+        height: 96%;
+
+        .select {
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          span {
+            background: rgba(0, 0, 0, 0.1);
+            border-radius: 12px;
+            padding: 5% 5%;
+            max-width: 60%;
+            min-width: 40%;
+            word-break: break-all;
+            text-align: center;
+          }
         }
+      }
+    }
+  }
+`;
 
-        .words{
-            
-            margin: 2% 2%;
-            width:96%;
-            display:flex;
-            flex-wrap:wrap;
+function BrainStorming() {
+  let json_data = {
+    root: "개",
+    개: ["리트리버", "푸들", "시츄", "말티즈", "웰시코기", "고양이"],
+    고양이: ["브리티시 숏헤어", "러시안 블루", "페르시안"],
+    리트리버: ["갈색", "대형견", "사냥"],
+    푸들: ["대형견", "흰색", "곱슬", "영국", "말티푸"],
+    시츄: ["소형견", "똑똑함", "조용함", "갈색", "흰색"],
+  };
 
-        }   
+  const [menu, setMenu] = useState(true);
+  const [word, setWord] = useState([]);
+  const [prev, setPrev] = useState("");
+  const [select, setSelect] = useState([json_data.root]);
+  const [print, setPrint] = useState([]);
+  const [click, setClick] = useState("단어를 선택해주세요");
+  const [enter, setEnter] = useState(false);
 
-        .tools{
-
-            margin: 2% 2%;
-            width:96%;
-            height:96%;
-
-            .select{
-
-                width:100%;
-                display:flex;
-                align-items:center;
-                justify-content:center;
-
-                span{
-                    
-                    background:rgba(0,0,0,0.1);
-                    border-radius:12px;
-                    padding: 5% 5%;
-                    max-width:60%;
-                    min-width:40%;
-                    word-break:break-all;
-                    text-align:center;
-                }
+  const renew_print_all = () => {
+    let buf = [];
+    for (let i = 0; i < 16; i++) {
+      if (buf.length < word.length) {
+        while (true) {
+          const rand = Math.floor(Math.random() * word.length);
+          let flag = false;
+          for (let j = 0; j < buf.length; j++) {
+            if (buf[j] === word[rand]) {
+              flag = true;
+              break;
             }
-        }
+          }
 
+          if (!flag) {
+            buf.push(word[rand]);
+            break;
+          }
+        }
+      } else {
+        buf.push(-1);
+      }
     }
 
+    setPrint(buf);
+  };
+
+  useEffect(() => {
+    setWord(json_data[json_data.root]);
+    setEnter(true);
+  }, []);
+
+  useEffect(() => {
+    if (word.length !== 0) renew_print_all();
+  }, [enter]);
+
+  useEffect(() => {
+    if (prev !== "") setSelect([...select, prev]);
+  }, [prev]);
+
+  return (
+    <BrainStormingCss menu={menu}>
+      <div className="word_select">
+        {print.map((e, idx) => {
+          return (
+            <PrintedWord
+              word={e !== -1 ? e : ""}
+              setPrev={setPrev}
+              key={idx}
+              json_data={json_data}
+              wordList={word}
+              setWord={setWord}
+              print={print}
+              setPrint={setPrint}
+            />
+          );
+        })}
+      </div>
+      <div className="menu">
+        <div className="title">
+          <p className="word" onClick={() => setMenu(false)}>
+            단어
+          </p>
+          <p className="tool" onClick={() => setMenu(true)}>
+            도구
+          </p>
+        </div>
+        <div className="slider">
+          <div className="container">
+            <div className="word_container">
+              <div className="words">
+                {select.map((e, idx) => {
+                  return (
+                    <Node
+                      word={e}
+                      setMenu={setMenu}
+                      key={idx}
+                      setClick={setClick}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="tools">
+              <div className="select">
+                <span>{click}</span>
+              </div>
+
+              <FontAwesomeIcon icon="fa-solid fa-chevron-right" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrainStormingCss>
+  );
 }
 
-`
-
-
-
-function BrainStorming(){
-
-    let json_data = {
-        root: "개",
-        개: ["리트리버", "푸들", "시츄", "말티즈", "웰시코기", "고양이"],
-        고양이: ["브리티시 숏헤어", "러시안 블루", "페르시안"],
-        리트리버: ["갈색", "대형견", "사냥"],
-        푸들:["대형견","흰색", "곱슬", "영국","말티푸"],
-        시츄:["소형견","똑똑함","조용함","갈색","흰색"]
-    };
-
-    const [menu,setMenu] = useState(true)
-    const [word,setWord] = useState([])
-    const [prev,setPrev] = useState("")
-    const [select,setSelect] = useState([json_data.root])
-    const [print,setPrint] = useState([])
-    const [click,setClick] = useState("단어를 선택해주세요")
-    const [enter,setEnter] = useState(false)
-
-    const renew_print_all=()=>{
-
-        let buf=[]
-        for(let i =0;i<16;i++){
-
-            if(buf.length<word.length){
-                while (true){
-                    const rand= Math.floor(Math.random() * word.length);
-                    let flag = false;
-                    for(let j=0;j<buf.length;j++ ){
-
-                        if(buf[j]===word[rand]){
-                            flag=true
-                            break;
-                        }
-                    }
-
-                    if(!flag){
-
-                        buf.push(word[rand])
-                        break
-                    }
-
-                }
-            }
-
-
-            else{
-                buf.push(-1)
-            }
-        }
-
-        setPrint(buf)
-
-    }
-
-    useEffect(()=>{
-
-        setWord(json_data[json_data.root]) 
-        setEnter(true)
-        
-    },[])
-
-    useEffect(()=>{
-
-        if(word.length!==0)
-          renew_print_all()
-
-    },[enter])
-
-    
-
-    useEffect(()=>{
-
-        if(prev!=="")
-            setSelect([...select,prev])
-
-    },[prev])
-
-    return(
-        <BrainStormingCss menu={menu}>
-            <div className="word_select">
-                {
-
-                    print.map((e,idx)=>{
-
-                        return(
-                            <PrintedWord word={e!==-1?e:""} setPrev={setPrev} key={idx}  json_data={json_data}
-                             wordList={word} setWord={setWord} print={print} setPrint={setPrint} 
-                            />
-                        )
-                    })
-
-                }
-            </div>
-            <div className="menu">
-                <div className="title">
-                    <p className="word" onClick={()=>setMenu(false)}>단어</p>
-                    <p className="tool" onClick={()=>setMenu(true)}>도구</p>
-                </div>
-                <div className="slider" >
-                    <div className="container" >
-
-                        <div className="word_container">
-                            <div className="words">
-
-                                {
-                                    
-                                    select.map((e,idx)=>{
-
-                                        return(<Node word={e} setMenu={setMenu} key={idx}  setClick={setClick} />)
-
-                                    })
-                                }
-                            </div>
-                        </div>
-                    
-                        <div className="tools">
-                           
-                            <div className="select">
-                                <span>    
-                                {click}
-                                </span>
-                            </div>
-
-                            <FontAwesomeIcon icon="fa-solid fa-chevron-right" />
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </BrainStormingCss>
-    )
-
-}
-
-
-export default BrainStorming
+export default BrainStorming;
