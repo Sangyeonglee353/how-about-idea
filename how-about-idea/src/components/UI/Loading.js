be20390d59a43d4808e5550031bc6cba5482a0fb
Sang Yeong Lee <Sangyeong.lee353@gmail.com>
2023-05-03 23:48:58 +0900

    Update speed of loading animation

--- how-about-idea/src/components/UI/Loading.js
+++ how-about-idea/src/components/UI/Loading.js
@@ -10,8 +10,8 @@ const LoadingCSS = styled.div`
   height: 1em;
   border-radius: 50%;
   text-indent: -9999em;
-  -webkit-animation: load5 1.1s infinite ease;
-  animation: load5 1.1s infinite ease;
+  -webkit-animation: load5 0.5s infinite ease;
+  animation: load5 0.5s infinite ease;
   -webkit-transform: translateZ(0);
   -ms-transform: translateZ(0);
   transform: translateZ(0);
